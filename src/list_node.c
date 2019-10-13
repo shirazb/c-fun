0fc604d397a3026013ec07209d97625d93fab639
Shiraz Butt <shiraz.b@icloud.com>
2019-10-13 19:58:49 +0100

    basic list node for ints only

---
+++ src/list_node.c
@@ -0,0 +1,35 @@
+#include <linked_list.h>
+
+#include <stdlib.h>
+
+struct list_node *
+list_node_init(struct list_node *node, int x) {
+    node->x = x;
+    node->next = NULL;
+
+    return node;
+}
+
+struct list_node *
+list_node_new(int x) {
+    struct list_node *node = malloc(sizeof(struct list_node));
+    if (!node) {
+        return node;
+    }
+
+    node->x = x;
+    node->next = NULL;
+
+    return node;
+}
+
+void
+list_node_deinit(struct list_node *node) {
+    node->next = NULL;
+}
+
+void
+list_node_destroy(struct list_node *node) {
+    list_node_deinit(node);
+    free(node);
+}
