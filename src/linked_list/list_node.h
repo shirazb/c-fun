3a66954d05be8c27342756a6d3b5441501236d54
Shiraz Butt <shiraz.b@icloud.com>
2019-11-08 01:11:42 +0000

    generify and move to  interface (header-only) library, though file still called list_node.h

---
+++ src/linked_list/list_node.h
@@ -0,0 +1,114 @@
+#ifndef _LINKED_LIST_H
+#define _LINKED_LIST_H
+
+
+#include <stdlib.h>
+
+#define list_node(TYPE) list_node_##TYPE
+#define list_node_elem_destroyer(TYPE) list_node_elem_destroyer_##TYPE
+#define list_node_init(TYPE) list_node_init_##TYPE
+#define list_node_new(TYPE) list_node_new_##TYPE
+#define list_node_deinit(TYPE) list_node_deinit_##TYPE
+#define list_node_destroy(TYPE) list_node_destroy_##TYPE
+/**
+ * Generates declarations and definitions of list_node struct for type `TYPE`.
+ * Example Usage (for ints):
+ * 
+ * GENERATE_LIST_NODE(int)
+ * 
+ * void f(void) {
+ *     // Initialiser function
+ *     struct list_node(int) x;
+ *     list_node_init(int)(x, 10);
+ *     
+ *     // Can also use return value if more convenient
+ *     struct list_node(int) y;
+ *     y = list_node_init(int)(y, 20);
+ * 
+ *     // New function that gives pointer to heap
+ *     struct list_node(int) *z = list_node_new(int)(30);
+ *     
+ *     x->next = &y;
+ *     y->next = z;
+ * 
+ *    // Can deinit an individual node, optionally passing in destroyer of
+ *    // element type
+ *    list_node_deinit(z, NULL);
+ *   
+ *    // Can destroy a node: will destroy `next`, deinit this node using the
+ *    // given element type destroyer, then free the memory of this node
+ *    list_node_destroy(x, NULL);
+ * }
+ */
+#define GENERATE_LIST_NODE(TYPE) \
+/* struct list_node */ \
+struct list_node(TYPE) { \
+    TYPE elem; \
+    struct list_node(TYPE) *next; \
+}; \
+\
+/* Prototypes */ \
+typedef void(* list_node_elem_destroyer(TYPE))(TYPE *); \
+\
+struct list_node(TYPE) * \
+list_node_init(TYPE)(struct list_node(TYPE) *node, TYPE elem); \
+\
+struct list_node(TYPE) * \
+list_node_new(TYPE)(TYPE elem); \
+\
+void \
+list_node_deinit(TYPE)( \
+        struct list_node(TYPE) *node, \
+        list_node_elem_destroyer(TYPE) elem_destroyer \
+); \
+\
+void \
+list_node_destroy(TYPE)( \
+        struct list_node(TYPE) *node, \
+        list_node_elem_destroyer(TYPE) elem_destroyer \
+); \
+\
+/* Definitions */ \
+struct list_node(TYPE) * \
+list_node_init(TYPE)(struct list_node(TYPE) *node, TYPE elem) { \
+    node->elem = elem; \
+    node->next = NULL; \
+    \
+    return node; \
+} \
+\
+struct list_node(TYPE) * \
+list_node_new(TYPE)(TYPE elem) { \
+    struct list_node(TYPE) *node = (struct list_node(TYPE) *) malloc(sizeof(struct list_node(TYPE))); \
+    if (!node) { \
+        return node; \
+    } \
+    \
+    return list_node_init(TYPE)(node, elem); \
+} \
+\
+void \
+list_node_deinit(TYPE)( \
+        struct list_node(TYPE) *node, \
+        list_node_elem_destroyer(TYPE) elem_destroyer \
+) { \
+    if (elem_destroyer) { \
+        elem_destroyer(&node->elem); \
+    } \
+    node->next = NULL; \
+} \
+\
+void \
+list_node_destroy(TYPE)( \
+        struct list_node(TYPE) *node, \
+        list_node_elem_destroyer(TYPE) elem_destroyer \
+) { \
+    if (node->next) { \
+        list_node_destroy(TYPE)(node->next, elem_destroyer); \
+    } \
+    list_node_deinit(TYPE)(node, elem_destroyer); \
+    free(node); \
+}
+
+
+#endif
--- test/test_list_node.c
+++ test/test_list_node.c
@@ -1,12 +1,28 @@
-#include <list_node.h>
+#include <linked_list/list_node.h>
 
-void test_list_node_blank(void) {
-    struct list_node *node = list_node_new(5);
-    node->next = list_node_new(10);
+#include <stdlib.h>
 
-    list_node_destroy(node);
-}
+typedef char * string;
+
+GENERATE_LIST_NODE(int)
+GENERATE_LIST_NODE(string)
+GENERATE_LIST_NODE(float)
+
+#define test_list_node_can_call_new_and_destroy(TYPE, NODE_ONE_ELEM, NODE_TWO_ELEM, ELEM_DESTROYER)\
+do { \
+    TYPE _val1 = (NODE_ONE_ELEM); \
+    TYPE _val2 = (NODE_TWO_ELEM); \
+    list_node_elem_destroyer(TYPE) _elem_destroyer = (ELEM_DESTROYER); \
+    \
+    struct list_node(TYPE) *_node = list_node_new(TYPE)(_val1); \
+    _node->next = list_node_new(TYPE)(_val2); \
+    \
+    list_node_destroy(TYPE)(_node, _elem_destroyer); \
+} while(0)
 
 int main(void) {
-    test_list_node_blank();
+    // Must have already generated list node for that type at top level.
+    test_list_node_can_call_new_and_destroy(int, 5, 10, NULL);
+    test_list_node_can_call_new_and_destroy(string, "hello", "why hello there", NULL);
+    test_list_node_can_call_new_and_destroy(float, 2.0e4, -3.0f, NULL);
 }
